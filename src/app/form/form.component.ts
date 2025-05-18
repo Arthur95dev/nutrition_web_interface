9fe1048353d813035a9808d0e73e6781b553e5ec
Артур Ефремов <artur_efremov_95@bk.ru>
2025-05-18 19:07:59 +0300

    Switch to angular

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    templateUrl: 'form.component.html',
+    styleUrl: 'form.component.scss'
+})
+
+class FormComponent {
+    
+}
+export default FormComponent;
